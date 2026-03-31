83c094859aa0f07d2536ae5f64d57555a0f874bb
Davide Del Bimbo <d.delbimbo@reply.it>
2026-03-31 08:32:30 +0200

    Initialize project

---
+++ src/main/java/it/reply/springboot_microservice_starter_kit/Application.java
@@ -0,0 +1,13 @@
+package it.reply.springboot_microservice_starter_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/reply/springboot_microservice_starter_kit/Application.java
@@ -0,0 +1,13 @@
+package it.reply.springboot_microservice_starter_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
